3e4a51d14b76b7e19933884793f577bb663af03d
Vishal <vishal.aditya6@gmail.com>
2021-11-21 01:17:23 +0530

    Implemented the Controller Methods. Closes #12

---
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
