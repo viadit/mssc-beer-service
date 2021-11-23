e9be14735fb936b78da40ff9c8585b4755141b79
Vishal <vishal.aditya6@gmail.com>
2021-11-23 19:37:50 +0530

    Configure JMS. closes #18

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-11-23T13:49:01+0530",
-    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+    date = "2021-11-23T19:29:14+0530",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
 )
 @Component
 @Primary
