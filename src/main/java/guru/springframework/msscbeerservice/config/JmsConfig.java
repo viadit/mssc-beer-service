f5a3b247db3452b0d157ccc3d86a97f4cd47a7a7
Vishal <vishal.aditya6@gmail.com>
2021-11-23 21:29:31 +0530

    Implement Brewing service to trigger Brewing. closes #20

--- src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/JmsConfig.java
@@ -9,6 +9,11 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
+    public static final String BREWING_REQUEST_QUEUE = "brewing-request";
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
+
     @Bean
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
